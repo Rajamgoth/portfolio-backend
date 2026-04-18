9cbc7d9199fabc2be3c79bf3ef35f4a34c0f3dfe
Rajkumar <amgothk50@gmail.com>
2026-04-18 16:43:14 +0530

    file changed

--- src/main/java/in/ashokit/RajkumarPortfolioAppApplication.java
+++ src/main/java/in/ashokit/RajkumarPortfolioAppApplication.java
@@ -2,8 +2,9 @@ package in.ashokit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class RajkumarPortfolioAppApplication {
 
 	public static void main(String[] args) {
