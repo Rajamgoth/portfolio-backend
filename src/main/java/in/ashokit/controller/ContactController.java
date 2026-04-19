e62f7e55c7d5413f01bc0fdbde6d464a3423cd03
Rajkumar <amgothk50@gmail.com>
2026-04-19 18:31:00 +0530

    file modified
    *ldf,l;
    
    ]

--- src/main/java/in/ashokit/controller/ContactController.java
+++ src/main/java/in/ashokit/controller/ContactController.java
@@ -3,8 +3,11 @@ package in.ashokit.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import in.ashokit.dto.ContactRequest;
 import in.ashokit.entity.Contact;
+import in.ashokit.repo.ContactRepository;
 import in.ashokit.service.EmailService;
 
 @RestController
@@ -21,6 +25,9 @@ public class ContactController {
 
 	@Autowired
 	private EmailService emailService;
+	
+	@Autowired
+	private ContactRepository contactRepository;
 
 	@PostMapping
 	public String sendMessage(@RequestBody ContactRequest request) {
@@ -31,5 +38,11 @@ public class ContactController {
 	@GetMapping("/messages")
 	public List<Contact> getAllMessages() {
 	    return emailService.getAllMessages();
+	}
+	
+	@DeleteMapping("/messages/{id}")
+	public ResponseEntity<String> deleteMessage(@PathVariable Long id) {
+	    contactRepository.deleteById(id);
+	    return ResponseEntity.ok("Deleted successfully");
 	}
 }
